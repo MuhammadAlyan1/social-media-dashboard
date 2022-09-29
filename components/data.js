85188d9fa3564258d937c3cf55693fde119a4158
Muhammad Alyan <alyan0332@gmail.com>
2022-09-29 23:37:07 +0500

    add data

---
+++ components/data.js
@@ -0,0 +1,90 @@
+import facebookIcon from '../public/icon-facebook.svg';
+import instagramIcon from '../public/icon-instagram.svg';
+import twitterIcon from '../public/icon-twitter.svg';
+import youtubeIcon from '../public/icon-youtube.svg';
+
+export const SocialMediaFansData = [
+  {
+    icon: facebookIcon,
+    username: '@MuhammadAlyan',
+    currentFans: 1987,
+    dailyChange: 12,
+    fansName: 'followers',
+    borderTop: 'bg-blue-400',
+  },
+  {
+    icon: twitterIcon,
+    username: '@AlyanDev',
+    currentFans: 1044,
+    dailyChange: 99,
+    fansName: 'followers',
+    borderTop: 'bg-blue-500',
+  },
+  {
+    icon: instagramIcon,
+    username: '@Alyan321',
+    currentFans: 11000,
+    dailyChange: 1099,
+    fansName: 'followers',
+    borderTop: 'instagram',
+  },
+  {
+    icon: youtubeIcon,
+    username: 'Alyan Developer',
+    currentFans: 8239,
+    dailyChange: -144,
+    fansName: 'subscribers',
+    borderTop: 'bg-red-500',
+  },
+];
+
+export const todaysData = [
+  {
+    icon: facebookIcon,
+    title: 'Page Views',
+    value: 87,
+    dailyChange: 3,
+  },
+  {
+    icon: facebookIcon,
+    title: 'Likes',
+    value: 52,
+    dailyChange: -2,
+  },
+  {
+    icon: instagramIcon,
+    title: 'Likes',
+    value: 5462,
+    dailyChange: 2257,
+  },
+  {
+    icon: instagramIcon,
+    title: 'Profile Views',
+    value: 52000,
+    dailyChange: 1375,
+  },
+  {
+    icon: twitterIcon,
+    title: 'Retweets',
+    value: 117,
+    dailyChange: 303,
+  },
+  {
+    icon: twitterIcon,
+    title: 'Likes',
+    value: 507,
+    dailyChange: 553,
+  },
+  {
+    icon: youtubeIcon,
+    title: 'Likes',
+    value: 107,
+    dailyChange: -19,
+  },
+  {
+    icon: youtubeIcon,
+    title: 'Total Views',
+    value: 1407,
+    dailyChange: -12,
+  },
+];
